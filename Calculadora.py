7003d21f9359595c1fa5696dc3ebe62b72bd140e
Djonathan Metelo <110343436+DjonathanMetelo@users.noreply.github.com>
2025-01-10 10:44:46 -0400

    Atualização do codigo
    adicionei aviso sobre divisão por zero e e controle para caso o usuario digite um numero ou caracter invalido

--- Calculadora.py
+++ Calculadora.py
@@ -1,119 +1,74 @@
-cont = 1
-while (int(cont) == 1):
-    print("")
-    print("Calculadora")
-    print("Escolha a opção desejada: ")
-    print("1 - Soma")
-    print("2 - Subtração")
-    print("3 - Multiplicação")
-    print("4 - Divisão")
-    print("5 - Sair")
-    opcao = input("Opção: ")
+def operacoes(opcao):
 
-    menuLoop = 1
+    while True:
 
-    if opcao == '1':
-        num1 = input("Insira o Primeiro Numero: ")
-        num2 = input("Insira o Segundo Numero:")
-        x = float(num1) + float(num2)
-        print("Resultado: ", float(x))
-        while (int(menuLoop) == 1):
-            menu = input("deseja voltar ao menu? (S/N) ")
-            if (menu == "s") or (menu == "S"):
-                print("Retornando ao menu...")
-                menuLoop = 0
-            elif (menu == "n") or (menu == "N"):
-                print("Finalizando o Programa...")
-                print("Volte Sempre!")
-                menuLoop = 0
-                cont = 0
-            else:
-                print("Opção Indisponivel")
+        try:
 
-    elif opcao == '2':
-        num1 = input("Insira o Primeiro Numero: ")
-        num2 = input("Insira o Segundo Numero:")
-        x = float(num1) - float(num2)
-        print("Resultado: ", float(x))
-        while (int(menuLoop) == 1):
-            menu = input("deseja voltar ao menu? (S/N) ")
-            if (menu == "s") or (menu == "S"):
-                print("Retornando ao menu...")
-                menuLoop = 0
-            elif (menu == "n") or (menu == "N"):
-                print("Finalizando o Programa...")
-                print("Volte Sempre!")
-                menuLoop = 0
-                cont = 0
-            else:
-                print("Opção Indisponivel")
-
-    elif opcao == '3':
-        num1 = input("Insira o Primeiro Numero: ")
-        num2 = input("Insira o Segundo Numero:")
-        x = float(num1) * float(num2)
-        print("Resultado: ", float(x))
-        while (int(menuLoop) == 1):
-            menu = input("deseja voltar ao menu? (S/N) ")
-            if (menu == "s") or (menu == "S"):
-                print("Retornando ao menu...")
-                menuLoop = 0
-            elif (menu == "n") or (menu == "N"):
-                print("Finalizando o Programa...")
-                print("Volte Sempre!")
-                menuLoop = 0
-                cont = 0
-            else:
-                print("Opção Indisponivel")
-
-    elif opcao == '4':
-        divZero = 0
-        while (int(divZero) == 0):
             num1 = input("Insira o Primeiro Numero: ")
             num2 = input("Insira o Segundo Numero:")
-            if (int(num2) == 0):
-                print("erro: divisão por zero")
-                opDiv = input("deseja fazer outra divisão? (S/N)")
-                if (opDiv == "s") or (opDiv == "S"):
-                    print("Entendido...")
-                elif (opDiv == "n") or (opDiv == "N"):
-                    while (int(menuLoop) == 1):
-                        menu = input("deseja voltar ao menu? (S/N) ")
-                        if (menu == "s") or (menu == "S"):
-                            print("Retornando ao menu...")
-                            divZero = 1
-                            menuLoop = 0
-                        elif (menu == "n") or (menu == "N"):
-                            print("Finalizando o Programa...")
-                            print("Volte Sempre!")
-                            divZero = 1
-                            menuLoop = 0
-                            cont = 0
-                        else:
-                            print("Opção Indisponivel")
-                else:
-                    print("Opção Indisponivel")
 
+            if opcao == '1':
+                resu = float(num1) + float(num2)
+                return resu
+
+            elif opcao == '2':
+                resu = float(num1) - float(num2)
+                return resu
+
+            elif opcao == '3':
+                resu = float(num1) * float(num2)
+                return resu
+
+            elif opcao == '4':
+                try:
+                    resu = float(num1) / float(num2)
+                    return resu
+                except ZeroDivisionError:
+                    print("erro: divisão por 0")
+
+        except ValueError:
+
+            print("Insira um numero Valido:")
+
+def loopmenu():
+
+    while True:
+        menu = input("deseja voltar ao menu? (S/N) ").strip().lower()
+        if menu == 's':
+            return True
+        elif menu == 'n':
+            return False
+        else:
+            print("Opção Indisponivel")
+
+def calculadora ():
+    while True:
+        print("")
+        print("Calculadora")
+        print("Escolha a opção desejada: ")
+        print("1 - Soma")
+        print("2 - Subtração")
+        print("3 - Multiplicação")
+        print("4 - Divisão")
+        print("5 - Sair")
+        opcao = input("Opção: ")
+
+        if opcao in {'1', '2', '3', '4'}:
+            resultados = operacoes(opcao)
+            print("resultado: ", resultados)
+            retorno = loopmenu()
+            if retorno:
+                print("Retornando ao menu...")
             else:
-                divZero = 1
-                x = float(num1) / float(num2)
-                print("Resultado: ", float(x))
-                while (int(menuLoop) == 1):
-                    menu = input("deseja voltar ao menu? (S/N) ")
-                    if (menu == "s") or (menu == "S"):
-                        print("Retornando ao menu...")
-                        menuLoop = 0
-                    elif (menu == "n") or (menu == "N"):
-                        print("Finalizando o Programa...")
-                        print("Volte Sempre!")
-                        menuLoop = 0
-                        cont = 0
-                    else:
-                        print("Opção Indisponivel")
+                print("Finalizando o Programa...")
+                print("Volte Sempre!")
+                break
 
-    elif opcao == '5':
-        print("Volte Sempre")
-        cont = 0
+        elif opcao == '5':
+            break
+        else:
+            print("Opção não disponivel")
 
-    else:
-        print("Opção não disponivel")
+
+
+calculadora()
